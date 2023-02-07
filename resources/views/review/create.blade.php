106fa01966dc433d7d84612e580480d9768ad52e
Galang Gema Maulid Akbar <galanggema92@gmail.com>
2023-02-07 19:45:42 +0700

    ok

--- resources/views/review/create.blade.php
+++ resources/views/review/create.blade.php
@@ -339,7 +339,7 @@
            },
            {
                "data": function (data) {
-                       return data[5];
+                       return '<span class="btn btn-ghost-success waves-effect waves-light btn-sm" onclick="modalLhp('+$data.id_rekom+')">Edit</span>';
                    },
                sDefaultContent: ""
            },
