f007da5c7467ac1db461eaf75211199606243c57
Galang Gema Maulid Akbar <galanggema92@gmail.com>
2023-02-08 12:40:36 +0700

    g

--- resources/views/review/create.blade.php
+++ resources/views/review/create.blade.php
@@ -49,7 +49,7 @@
                                                     // echo "<tr>";
                                                 }
                                                 echo "<td>" . $row[3] . "</td>";
-                                                echo "<td>" . $row[4] . "</td>";
+                                                // echo "<td>" . $row[4] . "</td>";
                                                 echo "</tr>";
                                             }
                                         ?>
