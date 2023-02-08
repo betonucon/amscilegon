3ae6e32f5f3fcf6afeb67b9e5322cd374ed48d21
Galang Gema Maulid Akbar <galanggema92@gmail.com>
2023-02-08 10:43:17 +0700

    ok

--- resources/views/review/create.blade.php
+++ resources/views/review/create.blade.php
@@ -37,8 +37,8 @@
                                         @foreach ($get as $g)
                                             <tr>
                                                 <td>{{ $no++ }}</td>
-                                                <td>{{ $g->file_lhp }}</td> 
-                                                <td rowspan="{{ group($g->id_rekom,$uraian_penyebab) }}">{{ $g->uraian_temuan }}</td>
+                                                <td>{{ $g->file_lhp }}</td>
+                                                <td rowspan="{{ group($g->id_rekom,$g->uraian_penyebab) }}">{{ $g->uraian_temuan }}</td>
                                                 <td>{{ $g->uraian_penyebab }}</td>
                                                 <td>{{ $g->uraian_rekomendasi }}</td>
                                             </tr>                                          
